2cb883f0cc94e13eedb627fe67d5b0f1e0d3c9c7
CatBoxy <thecatbox@protonmail.com>
2022-04-08 18:43:57 -0300

    typo

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout } from '../components/Layout';
+import { Layout } from '../components/layout';
 import { allPosts } from 'contentlayer/generated';
 import { pick } from 'utils/pick';
 import BlogPost from '../components/blogPost';
--- pages/works/botacura.jsx
+++ pages/works/botacura.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout } from '../../components/Layout';
+import { Layout } from '../../components/layout';
 import { BsGithub } from 'react-icons/bs';
 import Image from 'next/image';
 import botacuraImage from '../../public/works/Botacura.jpg';
