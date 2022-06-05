52d5a3f1d74e2c45ba48a8a76f7e512f338fa015
Juan <thecatbox@protonmail.com>
2022-06-05 14:42:14 -0700

    deploy fix

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -9,9 +9,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta property="og:title" content="Juan Cruz Lambrechts" />
-        <meta 
-          property="og:description"
-          content="FullStack web developer. I build my projects with React.js, Node.js, Styled-components, Redux, MySQL. Loving the craft." />
+        <meta property="og:description" content="FullStack web developer. I build my projects with React.js, Node.js, Styled-components, Redux, MySQL. Loving the craft." />
         <meta property="og:type" content="Website" />
         <meta property="og:url" content="https://personal-portfolio-catboxy.vercel.app/" />
         <meta name="author" content="Juan Cruz Lambrechts"></meta>
