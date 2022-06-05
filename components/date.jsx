31153f4feb0b5f6a347538b00252273b83f49d88
Juan <thecatbox@protonmail.com>
2022-06-05 16:59:35 -0700

    date digitalhouse fix

--- components/date.jsx
+++ components/date.jsx
@@ -1,5 +1,3 @@
-import { convertTime } from 'utils/convertTime';
-
 export default function Date({ dateString }) {
-  return <time dateTime={dateString}>{convertTime(dateString)}</time>;
+  return <time dateTime={dateString}>{dateString}</time>;
 }
--- utils/convertTime.js
+++
@@ -1,6 +0,0 @@
-export const convertTime = (time) =>
-  new Date(Date(time)).toLocaleDateString('es-AR', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  });
