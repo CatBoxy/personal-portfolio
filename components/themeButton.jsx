61b8f52569c0a92889f7e2d7797ea0c4fb2d8a3c
JuanCruzLamb <juancruz@torre.ai>
2024-01-03 19:31:11 -0300

    missing margin theme button

--- components/themeButton.jsx
+++ components/themeButton.jsx
@@ -9,7 +9,7 @@ export default function ThemeButton() {
     <>
       <button
         type="button"
-        className="w-9 h-9 dark:bg-gray-600 bg-gray-200 rounded-lg flex items-center justify-center hover:ring-2 ring-gray-300 transition-all"
+        className="mr-6 w-9 h-9 dark:bg-gray-600 bg-gray-200 rounded-lg flex items-center justify-center hover:ring-2 ring-gray-300 transition-all"
         onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
       >
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-800 dark:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
