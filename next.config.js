ef4b69a933ca51c5a2b063a6b935bfff5f1db256
JuanCruzLamb <juancruz@torre.ai>
2024-01-03 19:26:44 -0300

    new language data field

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const { withContentlayer } = require('next-contentlayer');
 
 module.exports = withContentlayer()({
+  i18n: {
+    locales: ['en', 'es'],
+    defaultLocale: 'en',
+  },
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com', 'i.imgur.com'],
